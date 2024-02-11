3cd95946a58135d3db5cc2d14ef75577e0cd0c2f
tanmaysingewar <68107378+tanmaysingewar@users.noreply.github.com>
2024-02-11 17:56:57 +0530

    page sequence change

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,8 @@ export default function Home() {
   return (
     <>
       <Header />
-      <AboutMegheGroup />
       <Departments />
+      <AboutMegheGroup />
       <Footer />
       {/* New Components will add here */}
     </>
