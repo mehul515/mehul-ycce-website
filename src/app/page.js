34904a90a463f0660319dd21e410add69140cc15
mehul515 <mehulkohad@gmail.com>
2024-02-14 23:31:01 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,19 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import { HeaderMenu } from "@/components/ui/HeaderMenu";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import AboutMegheGroup from "@/components/AboutMegheGroup";
 import Departments from "@/components/Departments";
 import Footer from "@/components/Footer";
+import DepartmentInfo from "@/components/DepartmentInfo";
 const logo = require("../../public/ycce_logo.png");
 
 export default function Home() {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <Departments />
+      <DepartmentInfo />
       <AboutMegheGroup />
       <Footer />
       {/* New Components will add here */}
